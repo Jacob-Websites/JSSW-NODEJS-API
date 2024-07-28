bff7ab0dad93e6f4d20bfb2c0ed23677fcf06922
prabhutejad <prabhuteja.iot@gmail.com>
2024-07-28 16:47:11 +0530

    GetQuery added

--- index.js
+++ index.js
@@ -58,22 +58,53 @@ app.post('/api/addorganization', (req, res) => {
 
 });
 
-app.get('/api/Organizations',(req,res)=>{
-  pool.query(`select * from Organization`,(err,result)=>{
-    if(err){
-      console.error("Error Fetching Organization")
-      res.json({
-        status:403,
-        error:"Error Fetching Records"
-      })
-    }else{
-      res.json({
-        status:200,
-        data:result
-      })
+
+
+app.get('/api/Organization', (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 5;
+
+  const offset = (page - 1) * pageSize;
+
+  const query = `SELECT * FROM Organization LIMIT ? OFFSET ?`;
+
+  const countQuery = `SELECT COUNT(*) AS total FROM Organization`;
+
+  pool.query(countQuery, (err, countResult) => {
+    if (err) {
+      console.error("Error Fetching Organization Count");
+      return res.json({
+        status: 403,
+        error: "Error Fetching Records"
+      });
+    } else {
+      const totalRecords = countResult[0].total;
+      const totalPages = Math.ceil(totalRecords / pageSize);
+
+      pool.query(query, [pageSize, offset], (err, result) => {
+        if (err) {
+          return res.json({
+            status: 403,
+            error:err
+          });
+        } else {
+          res.json({
+            status: 200,
+            data: {
+              result,
+              totalRecords,
+              totalPages,
+              currentPage: page,
+              pageSize
+            },
+           
+          });
+        }
+      });
     }
-  })
-})
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}.`);
